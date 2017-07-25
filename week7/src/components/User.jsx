1b35eb8fcf2030ce1da09a68be83eb77c8da3fb2
Miguel Diaz <MiguelDiazDesigner@gmail.com>
2017-07-24 21:32:06 -0400

    Adding React

---
+++ week7/src/components/User.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './User.css'
+
+class User extends Component {
+  render() {
+    const {
+      name,
+      email,
+      username,
+      quote
+    } = this.props;
+    return (
+      <div className="User">
+      <ul>
+        <li><h2>{name}</h2></li>
+        <li><a href={`mailto:${email}`}>{email}</a></li>
+        <li className="username">{username}</li>
+      </ul>
+      <blockquote>
+        <p>{quote}</p>
+        <small>{name}</small>
+      </blockquote>
+      </div>
+
+    )
+  }
+}
+
+User.defaultProps = {
+  name: 'Full Name',
+  email: 'Me@You.com',
+  username: 'username123',
+  quote: 'my Quote is better than yours.'
+}
+
+
+export default User;
